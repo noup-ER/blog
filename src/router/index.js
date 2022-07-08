7a78f943bad3d148eda0e2eb83547f7478a4bd58
ekko <759270724@qq.com>
2022-07-08 16:35:38 +0800

    :bug: 修复重复点击路由报错

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import router_url from "./router_url";
 
 Vue.use(Router);
 
+//防止重复点击路由报错
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 var router = new Router({
   mode: process.env.NODE_ENV === "development"?"hash":"history",
